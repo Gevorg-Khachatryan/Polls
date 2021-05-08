40d8e05b4041a46f882888c6c069ba75436d9719
Gevorg Khachatryan <kgevorg97@gmail.com>
2021-05-08 23:08:58 +0400

    first commit

--- poll/settings.py
+++ poll/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mht0e==&dscyym(-@2%!p&t(1n^(uguisa+(^3jfl$%z-i!t47'
+SECRET_KEY = '...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
